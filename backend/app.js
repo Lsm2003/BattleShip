91d0d323538f1e825476544b99fc8efaf2d4cfa0
Lsm2003 <Lucasmaloney2003@gmail.com>
2025-04-13 15:13:28 -0230

    removed unused files

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,5 @@
 const express = require('express')
 const app = express()
-// const newRoute = require ("./routes/new.route.js");
-// const lobRoute = require ("./routes/lob.route.js");
-// const missRoute = require ("./routes/miss.route.js");
-// const hitRoute = require ("./routes/hit.route.js");
-// const concedeRoute = require ("./routes/concede.route.js");
-// const cancelRoute = require ("./routes/cancel.route.js");
-// const statusRoute = require ("./routes/status.route.js");
 const battleshipRoute = require ("./routes/battleship.route.js");
 const port = 3000
 const cors = require('cors');
@@ -17,13 +10,6 @@ app.listen(port, () => {
     console.log(`BattleShip app listening on port ${port}`)
   })
 
-// app.use ("/battleship/new", newRoute);
-// app.use ("/battleship/lob", lobRoute);
-// app.use ("/battleship/miss", missRoute);
-// app.use ("/battleship/hit", hitRoute);
-// app.use ("/battleship/concede", concedeRoute);
-// app.use ("/battleship/cancel", cancelRoute);
-// app.use ("/battleship/status", statusRoute);
 app.use ("/battleship", battleshipRoute);
 
 app.get('/', (req, res, next) => {
